266830bc742f56858b1ab9831f25ef243e2d0fda
Ryan Cheley <rcheley@gmail.com>
2021-03-08 07:25:34 -0800

    added real tests for the project

--- tests/test_toggl_to_sqlite.py
+++ tests/test_toggl_to_sqlite.py
@@ -1,4 +1,40 @@
+from toggl_to_sqlite import utils
 import pytest
+import json
+import sqlite_utils
+from sqlite_utils.db import ForeignKey
+import pathlib
 
-def test_trivial_test():
-    assert 1 == 1
+
+def load():
+    json_path = pathlib.Path(__file__).parent / "toggl.json"
+    return json.load(open(json_path, "r"))
+
+
+@pytest.fixture(scope="session")
+def converted():
+    db = sqlite_utils.Database(":memory:")
+    utils.save_items(load(), db)
+    return db
+
+
+def test_tables(converted):
+    assert {'time_entries'} == set(converted.table_names())
+
+
+def test_item(converted):
+    item = list(converted["time_entries"].rows)[0]
+    print(item)
+    # assert 1 == 0
+    assert {
+            "id":436691234,
+            "wid":777,
+            "pid":123,
+            "billable":1,
+            "start":"2013-03-11T11:36:00+00:00",
+            "stop":"2013-03-11T15:36:00+00:00",
+            "duration":14400,
+            "description":"Meeting with the client",
+            "tags": '["tag1, tag2"]',
+            "at":"2013-03-11T15:36:58+00:00"
+        } == item
